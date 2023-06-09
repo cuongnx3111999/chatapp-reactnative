106acde2f6a4db5e32d8fbd88411c0ccd6e432ea
Cuong <44018211+cuongnx3111999@users.noreply.github.com>
2023-06-09 22:58:06 +0700

    login amplify

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigator from './src/navigation';
+import {Amplify} from "aws-amplify";
+import {withAuthenticator} from "aws-amplify-react-native/src";
+import awsconfig from "./src/aws-exports";
 
-export default function App() {
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
+
+function App() {
   return (
     <View style={styles.container}>
       <Navigator />
@@ -19,3 +24,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+export default withAuthenticator(App);
